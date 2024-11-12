a684930eaf912be6dd77b5f3275429ebeaba6d6c
Saksham Agarwal <107304333+Sakshamag21@users.noreply.github.com>
2024-11-12 18:32:46 +0530

    Added cors

--- app.js
+++ app.js
@@ -7,36 +7,35 @@ const users = require("./models/userSchema");
 const cors = require("cors");
 const resourcesRouter = require("./routes/resourcesRouter");
 const resultsRouter = require("./routes/resultsRouter");
-const mailRouter=require("./routes/mail")
-const attendanceRouter= require("./routes/attendanceRoutes")
-const userRouter= require("./routes/userRouter")
-const scheduleRouter= require("./routes/scheduleRouter")
+const mailRouter = require("./routes/mail");
+const attendanceRouter = require("./routes/attendanceRoutes");
+const userRouter = require("./routes/userRouter");
+const scheduleRouter = require("./routes/scheduleRouter");
 
 const port = process.env.PORT || 8003;
 
-app.use(cors());
+// Set up CORS to allow only specific origin
+app.use(cors({
+    origin: "https://victors-frontend.vercel.app"
+}));
+
 app.use(express.json());
 
-// app.get("/",(req,res)=>{
-//     res.json("server start")
-// })
+app.use("/resources", resourcesRouter);
+app.use("/results", resultsRouter);
+app.use("/mail", mailRouter);
+app.use("/attendance", attendanceRouter);
+app.use("/user", userRouter);
+app.use("/schedule", scheduleRouter);
 
-app.use("/resources",resourcesRouter);
-app.use("/results",resultsRouter);
-app.use("/mail",mailRouter);
-app.use("/attendance",attendanceRouter);
-app.use("/user",userRouter)
-app.use("/schedule",scheduleRouter)
-
-if(process.env.NODE_ENV=='production'){
-    const path = require('path')
-
-    app.get('/',(req,res)=>{
-        app.use(express.static(path.resolve(__dirname,'client','build')))
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
+if (process.env.NODE_ENV == 'production') {
+    const path = require('path');
+    app.get('/', (req, res) => {
+        app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 app.listen(port, () => {
-    console.log(`server is start port number ${port}`);
+    console.log(`Server is running on port number ${port}`);
 });
